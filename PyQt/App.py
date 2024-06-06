db80e2180789a8e2c7b4ac4089a417e82d7ef15c
SSeelos <sebastian.seelos@gmx.de>
2024-06-06 17:28:38 +0200

    mainwindow

--- PyQt/App.py
+++ PyQt/App.py
@@ -2,6 +2,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+class MainWindow(QMainWindow):
+    pass
 def Run(args):
     app = QApplication(args)
+    window = MainWindow()
+    window.show()
     app.exec_()
