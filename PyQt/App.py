2b75365d82087148aa9d6c0bdbc1cc5201b57c33
SSeelos <sebastian.seelos@gmx.de>
2024-06-10 13:45:34 +0200

    set_interface

--- PyQt/App.py
+++ PyQt/App.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 class MainWindow(QMainWindow):
-    button: QPushButton
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
@@ -11,22 +10,25 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(600, 400)
 
         self.label = QLabel("myLabel")
+        self.button = QPushButton("MyButtonA")
         self.set_layout()
+        self.set_interface()
 
         self.show()
+
+
     def set_layout(self):
-
-        self.button = QPushButton("MyButtonA")
-        self.button.setCheckable(True)
-        self.button.clicked.connect(self.button_clicked)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.button)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.button)
 
         container = QWidget()
-        container.setLayout(layout)
+        container.setLayout(self.layout)
         self.setCentralWidget(container)
+
+    def set_interface(self):
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.button_clicked)
 
     def button_clicked(self):
         print("clicked")
