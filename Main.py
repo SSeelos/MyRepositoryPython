b40b87c09d545ac4a45dfc9540ce5fb535e4d5e2
SSeelos <sebastian.seelos@gmx.de>
2024-06-10 16:30:48 +0200

    if __name__ == '__main__':

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 import sys
 import PyQt.App
 
-try:
-    PyQt.App.Run(sys.argv)
-except Exception as e:
-    print("you've done fucked up")
-    print(e)
-else:
-    print("at least you tried")
-finally:
-    print("I am done")
+if __name__ == '__main__':
+    try:
+        PyQt.App.run(sys.argv)
+    except Exception as e:
+        print("you've done fucked up")
+        print(e)
+    else:
+        print("at least you tried")
+    finally:
+        print("I am done")
