758b80270166abcf7549673bc7e20ed2567ed310
SSeelos <sebastian.seelos@gmx.de>
2024-06-10 16:31:41 +0200

    openview btn, sys.exit

--- PyQt/App.py
+++ PyQt/App.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -11,16 +13,17 @@ class MainWindow(QMainWindow):
 
         self.label = QLabel("myLabel")
         self.button = QPushButton("MyButtonA")
+        self.open_view_btn = QPushButton("OpenView")
+        self.layout = QVBoxLayout()
         self.set_layout()
         self.set_interface()
 
         self.show()
 
-
     def set_layout(self):
-        self.layout = QVBoxLayout()
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.button)
+        self.layout.addWidget(self.open_view_btn)
 
         container = QWidget()
         container.setLayout(self.layout)
@@ -30,12 +33,15 @@ class MainWindow(QMainWindow):
         self.button.setCheckable(True)
         self.button.clicked.connect(self.button_clicked)
 
+        self.open_view_btn.clicked.connect(self.open_view)
+
     def button_clicked(self):
         print("clicked")
 
+    def open_view(self):
+        print("open_view")
 
-
-def Run(args):
+def run(args):
     app = QApplication(args)
     window = MainWindow()
-    app.exec_()
+    sys.exit(app.exec_())
